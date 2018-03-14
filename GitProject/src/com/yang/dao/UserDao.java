224507d7a66685746cf52b833bb23842c1450723
jenni <jenni@naver.com>
2018-03-14 15:39:08 +0900

    Ver 3

--- GitProject/src/com/yang/dao/UserDao.java
+++ GitProject/src/com/yang/dao/UserDao.java
@@ -1,5 +1,18 @@
 package com.yang.dao;
 
-public class UserDao {
+import java.util.ArrayList;
+import java.util.List;
 
+import com.yang.entiy.User;
+
+public class UserDao {
+	
+	List<User> list = null;
+	
+	public UserDao() {
+		list.add(new User("홍길동",17,75.3));
+		list.add(new User("이순신",18,100));
+		list.add(new User("강호동",15,90.3));
+		
+	}
 }
