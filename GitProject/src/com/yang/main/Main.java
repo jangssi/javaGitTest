bc1a29db2e072a1b61b98260c27b1201897a2422
jang_Ssi <poiuy4600@naver.com>
2018-03-14 15:52:19 +0900

    Ver 2

--- GitProject/src/com/yang/main/Main.java
+++ GitProject/src/com/yang/main/Main.java
@@ -1,10 +1,21 @@
 package com.yang.main;
 
+
+import java.util.List;
+
+import com.yang.dao.UserDao;
+import com.yang.entiy.User;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		UserDao dao = null;
+		List<User> list = dao.getUserList();
+		
+		for (User user : list) {
+			System.out.println(user.getName()+" "+user.age()+" "user.score());
+		}
 	}
 
 }
