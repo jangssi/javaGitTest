374789d66ee21978ff650849415fb5d2979d6d62
jang_Ssi <poiuy4600@naver.com>
2018-03-14 15:56:59 +0900

    Ver 4

--- GitProject/src/com/yang/dao/UserDao.java
+++ GitProject/src/com/yang/dao/UserDao.java
@@ -10,9 +10,14 @@ public class UserDao {
 	List<User> list = null;
 	
 	public UserDao() {
+		list = new ArrayList<>();
 		list.add(new User("홍길동",17,75.3));
 		list.add(new User("이순신",18,100));
 		list.add(new User("강호동",15,90.3));
 		
+	}
+	public List<User> getUserList(){
+		new UserDao();
+		return list;
 	}
 }
--- GitProject/src/com/yang/main/Main.java
+++ GitProject/src/com/yang/main/Main.java
@@ -10,11 +10,11 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		UserDao dao = null;
+		UserDao dao = new UserDao();
 		List<User> list = dao.getUserList();
 		
 		for (User user : list) {
-			System.out.println(user.getName()+" "+user.age()+" "user.score());
+			System.out.println(user.getName()+" "+user.getAge()+" "+user.getScore());
 		}
 	}
  
